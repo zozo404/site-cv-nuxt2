0f3780984a786799597c87d93877fdb15631c4b0
zozo404 <enzocuny66@gmail.com>
2022-09-14 12:26:55 +0200

    💩 | slug services&reservationCard

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div v-if="game" class="flex flex-col items-center mt-10 gap-4">
+    <div class="flex flex-col items-center mt-10 gap-4">
       <Titles :title="game.name" />
       <iframe
         class="w-5/6 h-96 xl:w-1/2 border-2 border-pink-500"
@@ -17,26 +17,6 @@
         <p>{{ game.text }}</p>
       </div>
     </div>
-    <div v-if="service" class="flex flex-col items-center mt-10 gap-4">
-      <nuxt-link class="font-light text-xl px-2 rounded-xl border border-transparent hover:border-pink-500" to="/reservation">
-        Retour
-      </nuxt-link>
-      <Titles :title="service.name" />
-      <div class="px-5 flex flex-wrap justify-center md:flex-col xl:flex-row md:items-center md:justify-around gap-4 w-full">
-        <div class="flex flex-col items-center text-center lg:w-1/2 xl:w-auto">
-          <nuxt-img
-            provider="sanity"
-            :src="service.imageUrl.asset._ref"
-            placeholder
-            class="h-52 w-full xl:w-auto lg:w-4/5 rounded-xl"
-          />
-          <p class="pt-3">
-            {{ service.text }}
-          </p>
-        </div>
-        <DatePicker />
-      </div>
-    </div>
   </div>
 </template>
 <script>
@@ -44,15 +24,12 @@ import { mapGetters } from 'vuex'
 export default {
   name: 'SlugPage',
   computed: {
-    ...mapGetters(['getGames', 'getServices']),
+    ...mapGetters(['getGames']),
     routeSlug () {
       return this.$route.params.slug
     },
     game () {
       return this.getGames.find(el => el.slug.current === this.routeSlug)
-    },
-    service () {
-      return this.getServices.find(el => el.slug.current === this.routeSlug)
     }
   }
 }
--- store/index.js
+++ store/index.js
@@ -1,8 +1,7 @@
 export const state = () => ({
   games: [],
   pages: [],
-  editors: [],
-  services: []
+  editors: []
 })
 
 export const getters = {
@@ -14,9 +13,6 @@ export const getters = {
   },
   getEditors (state) {
     return state.editors
-  },
-  getServices (state) {
-    return state.services
   }
 }
 
@@ -29,9 +25,6 @@ export const mutations = {
   },
   SET_EDITORS (state, editors) {
     state.editors = editors
-  },
-  SET_SERVICES (state, services) {
-    state.services = services
   }
 }
 
@@ -43,12 +36,9 @@ export const actions = {
 
     const editors = await $axios.$get('https://byaeh17d.api.sanity.io/v2021-03-25/data/query/production?query=*[_type == "editors"]{name,"imageUrl":image{asset},_id,link}')
 
-    const services = await $axios.$get('https://byaeh17d.api.sanity.io/v2021-03-25/data/query/production?query=*[_type == "services"]{name,"imageUrl":image{asset},_id,slug,text,url}')
-
     await dispatch('setGames', games.result)
     await dispatch('setPages', pages.result)
     await dispatch('setEditors', editors.result)
-    await dispatch('setServices', services.result)
   },
   setGames ({ commit }, games) {
     commit('SET_GAMES', games)
@@ -58,8 +48,5 @@ export const actions = {
   },
   setEditors ({ commit }, editors) {
     commit('SET_EDITORS', editors)
-  },
-  setServices ({ commit }, services) {
-    commit('SET_SERVICES', services)
   }
 }
