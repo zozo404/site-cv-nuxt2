63258f1f6f4d9595c141111629bff26d9cbcc098
zozo404 <enzocuny66@gmail.com>
2022-09-14 12:32:56 +0200

    ♻️ | upgrade slug

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -3,7 +3,7 @@
     <div class="flex flex-col items-center mt-10 gap-4">
       <Titles :title="game.name" />
       <iframe
-        class="w-5/6 h-96 xl:w-1/2 border-2 border-pink-500"
+        class="w-5/6 h-96 xl:w-1/2 border-2 border-pink-500 rounded-xl"
         :src="`https://www.youtube.com/embed/${game.youtube}`"
         title="YouTube video player"
         frameborder="0"
@@ -11,10 +11,14 @@
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       />
 
-      <div class="px-5 text-centerw-1/2">
-        <p>{{ game.players }} {{ game.difficulty }}</p>
-        <br>
-        <p>{{ game.text }}</p>
+      <div class="mt-4 px-5 flex flex-col text-center w-1/2">
+        <div class="flex justify-center space-x-4">
+          <span class="inline-flex items-center rounded-full bg-gray-100 px-3 py-0.5 text-sm font-medium text-gray-800">{{ game.players }}</span>
+          <span class="inline-flex items-center rounded-full bg-red-100 px-3 py-0.5 text-sm font-medium text-red-800">{{ game.difficulty }}</span>
+        </div>
+        <p class="mt-8">
+          {{ game.text }}
+        </p>
       </div>
     </div>
   </div>
@@ -33,5 +37,4 @@ export default {
     }
   }
 }
-
 </script>
