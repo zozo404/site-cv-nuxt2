cfdd67f4f16df2cb7b98d7576f843893f40f41c5
zozo404 <enzocuny66@gmail.com>
2022-11-09 18:33:37 +0100

    ⚡️ |link button link

--- components/ButtonLink.vue
+++ components/ButtonLink.vue
@@ -2,7 +2,7 @@
   <div class="flex justify-center">
     <!-- button that links to all my repositories on github -->
     <p class="shadow-md duration-200 hover:scale-105 hover:shadow-xl">
-      <a href="https://github.com/zozo404/site-cv-nuxt2" rel="noopener" target="_blank" class="bg-cyan-400 p-2 rounded-md text-black ">
+      <a href="https://github.com/zozo404?tab=repositories" rel="noopener" target="_blank" class="bg-cyan-400 p-2 rounded-md text-black ">
         Voir plus
       </a>
     </p>
