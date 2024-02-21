f0f6137783a7ea74244cb32fe128c28fb61b2d1c
zozo404 <enzocuny66@gmail.com>
2024-02-21 16:01:48 +0100

    💄 | new logo

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="bg-zinc-900 lg:h-40 w-full z-20 sticky top-0 fade-scroll" :class="{ visible: isVisible }">
     <nav
-      class="2lg:max-h-40 max-h-20 w-full py-4 2lg:py-6 grid grid-cols-2 grid-rows-1 2lg:grid-cols-3 items-center justify-between 2lg:border-none bg-zinc-900"
+      class="2lg:max-h-40 max-h-20 SamsungGA425G:max-h-24 w-full py-4 2lg:py-6 grid grid-cols-2 grid-rows-1 2lg:grid-cols-3 items-center justify-between 2lg:border-none bg-zinc-900"
       aria-label="Top"
     >
       <!-- logo -->
@@ -38,7 +38,7 @@
     </nav>
     <!-- nav ouverte -->
     <nav :class="{hidden:isActive}" class="2lg:hidden w-full absolute top-0 h-screen z-40 bg-zinc-900 grid grid-cols-2 grid-rows-6 ">
-      <div class="grid grid-cols-2 grid-rows-1 col-span-2  my-4 h-12 justify-items-center w-full">
+      <div class="grid grid-cols-2 grid-rows-1 col-span-2  my-4 h-12 SamsungGA425G:h-16 justify-items-center w-full">
         <!-- logo -->
         <nuxt-link to="/" class="flex justify-center lg:col-span-1 h-full ">
           <span class="sr-only">Logo</span>
